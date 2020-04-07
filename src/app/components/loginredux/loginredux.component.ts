3b8bc70beef56ef2655b6326e83e1f27cbf70cb7
Camberden <bernsteincuevas@outlook.com>
2020-04-07 14:35:17 -0400

    First push of loginredux

---
+++ src/app/components/loginredux/loginredux.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginredux',
+  templateUrl: './loginredux.component.html',
+  styleUrls: ['./loginredux.component.css']
+})
+export class LoginreduxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
