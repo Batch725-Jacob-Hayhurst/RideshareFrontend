99fc2e5622d23a44da3ffa133d3605dc28955ef0
Sergiogamboa11 <sergio.gamboa11@gmail.com>
2020-04-14 11:34:50 -0600

    Fixed issue where total seats were not being shown on driver table

--- src/app/components/driver-list/driver-list.component.ts
+++ src/app/components/driver-list/driver-list.component.ts
@@ -30,7 +30,7 @@ export class DriverListComponent implements OnInit {
   availableCars: Array<any> = [];
   drivers: Array<Driver> = [];
   isLoading = true;
-  displayedColumns: string[] = ['name', 'distance', 'time', 'spots', 'view'];
+  displayedColumns: string[] = ['name', 'distance', 'time', 'seats', 'totalseats', 'view'];
   dataSource = new MatTableDataSource<Driver>();
 
 
