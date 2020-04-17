c9c57d98eeb2539d341f4b22b4a60daaffc9c34e
captainajm12 <captainajm12@gmail.com>
2020-04-17 08:46:24 -0500

    Work from 18/04

--- e2e/codecept/signup_test.js
+++ e2e/codecept/signup_test.js
@@ -13,37 +13,46 @@ Feature('sign-up');
 // });
 
 // doesn't work yet
-// Scenario('testing valid last name', (I) => {
-//     I.amOnPage('/');
-//     I.click('#signupid');
-//     I.appendField('First Name *', 'Timoth');
-//     I.appendField('Last Name *', 'M');
-//     I.clearField('Last Name *');
-//     I.appendField('First Name *', 'y');
-//     I.see('*Required Field');
-//     I.fillField('Last Name *', 'Mitchell');
-//     I.dontSee('*Required Field');
-// });
-
-Scenario('testing valid email', (I) => {
+Scenario('testing valid last name', (I) => {
     I.amOnPage('/');
     I.click('#signupid');
-    I.appendField('Email *', 'dslkfjsdf');
-    I.appendField('Last Name *', 'Mitchell');
-    I.see('*Required. Email Not Valid.');
-    I.clearField('Email *');
-    I.appendField('Email *', 'mitchelltim29@gmail');
+    I.appendField('First Name *', 'Timoth');
+    I.appendField('Last Name *', 'MitchMan');
+    // I.wait(10);
+    I.clearField('First Name *');
+    I.fillField('First Name *', '');
     I.clearField('Last Name *');
-    I.appendField('Last Name *', 'Mitchell');
-    I.see('*Required. Email Not Valid.');
-    I.clearField('Email *');
-    I.appendField('Email *', 'mitchelltim29.gmail');
-    I.clearField('Last Name *');
-    I.appendField('Last Name *', 'Mitchell');
-    I.see('*Required. Email Not Valid.');
-    I.clearField('Email *');
-    I.appendField('Email *', 'mitchelltim29@gmail.com');
-    I.clearField('Last Name *');
-    I.appendField('Last Name *', 'Mitchell');
-    I.dontSee('*Required. Email Not Valid.');
+    I.fillField('Last Name *', '');
+    // I.wait(20);
+    // I.click('Last Name *');
+    // I.appendField('Last Name *', '');
+    I.click('#logo');
+    I.see('*Required Field');
+    // I.appendField('First Name *', 'y');
+    I.fillField('Last Name *', 'Mitchell');
+    // I.click('#logo');
+    I.dontSee('*Required Field');
 });
+
+// Scenario('testing valid email', (I) => {
+//     I.amOnPage('/');
+//     I.click('#signupid');
+//     I.appendField('Email *', 'dslkfjsdf');
+//     I.appendField('Last Name *', 'Mitchell');
+//     I.see('*Required. Email Not Valid.');
+//     I.clearField('Email *');
+//     I.appendField('Email *', 'mitchelltim29@gmail');
+//     I.clearField('Last Name *');
+//     I.appendField('Last Name *', 'Mitchell');
+//     I.see('*Required. Email Not Valid.');
+//     I.clearField('Email *');
+//     I.appendField('Email *', 'mitchelltim29.gmail');
+//     I.clearField('Last Name *');
+//     I.appendField('Last Name *', 'Mitchell');
+//     I.see('*Required. Email Not Valid.');
+//     I.clearField('Email *');
+//     I.appendField('Email *', 'mitchelltim29@gmail.com');
+//     I.clearField('Last Name *');
+//     I.appendField('Last Name *', 'Mitchell');
+//     I.dontSee('*Required. Email Not Valid.');
+// });
