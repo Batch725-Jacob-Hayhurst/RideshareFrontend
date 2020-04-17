2aa21efbed9234ed18239d27cb2c8edcb6ccd0ae
EricTBKelley <ericthomaskelley@gmail.com>
2020-04-17 11:15:40 -0500

    working on username validation

--- src/app/directives/usernamecheck/username-check.directive.ts
+++ src/app/directives/usernamecheck/username-check.directive.ts
@@ -2,23 +2,23 @@ import { Directive } from '@angular/core';
 import { AbstractControl, NG_VALIDATORS, Validator, ValidationErrors, ValidatorFn, FormGroup } from '@angular/forms';
 import { UserService } from '../../services/user-service/user.service';
 
-// export const usernameMatchValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
-//   const userValid = control.get('userName');
-
-//   return password && confirm && password.value !== confirm.value ? {'notSame': true} : null;
-// };
-
 
 @Directive({
-  selector: '[appUsernameCheck]'
+  selector: '[appUsernameCheck]',
+  providers: [{provide: NG_VALIDATORS, useExisting: UsernameCheckDirective, multi: true}]
 })
 export class UsernameCheckDirective implements Validator{
 
   constructor(private userServ: UserService) { }
-  async validate(control: AbstractControl){
+  validate(control: AbstractControl){
+    return this.usernameMatchValidator(control);
+  }
+
+  usernameMatchValidator: ValidatorFn = (control: FormGroup): ValidationErrors | null => {
+    const userValid = control.get('userName');
     let result: boolean;
-    await this.userServ.checkUserNameAvailable(control.get('userName') as unknown as string).toPromise().then(res => {result = res});
-    return result;
+    this.userServ.checkUserNameAvailable(control.get('userName') as unknown as string).toPromise().then(res => {result = res});
+    return result ? null : {'unavailable':true};
   }
 
 }
