b82a7393408837120435b5f5afb7e5c5a54db038
MitchellTim29 <mitchelltim29@gmail.com>
2020-04-17 13:20:38 -0400

    testing done except for needing username validation and sign up form submission to work

--- e2e/codecept/signup_test.js
+++ e2e/codecept/signup_test.js
@@ -12,19 +12,6 @@ Feature('sign-up');
 //     I.dontSee('*Required Field');
 // });
 
-// DOESN'T WORK YET
-// Scenario('testing valid last name', (I) => {
-//     I.amOnPage('/');
-//     I.click('#signupid');
-//     I.appendField('First Name *', 'Timoth');
-//     I.appendField('Last Name *', 'M');
-//     I.clearField('Last Name *');
-//     I.appendField('First Name *', 'y');
-//     I.see('*Required Field');
-//     I.fillField('Last Name *', 'Mitchell');
-//     I.dontSee('*Required Field');
-// });
-
 // Scenario('testing valid email', (I) => {
 //     I.amOnPage('/');
 //     I.click('#signupid');
@@ -66,7 +53,6 @@ Feature('sign-up');
 //     I.dontSee('*10 Digit Phone-numbers only');
 // });
 
-// DOESN'T WORK YET
 // Scenario('testing valid address', (I) => {
 //     I.amOnPage('/');
 //     I.click('#signupid');
@@ -77,70 +63,67 @@ Feature('sign-up');
 //     I.appendField('Zip Code *', '29642');
 //     I.click('Work Location');
 //     I.click('#city');
-//     I.click('Register');
 //     I.see('That address does not exist!');
 //     I.clearField('Home Address');
 //     I.clearField('City *');
 //     I.clearField('Zip Code *');
-//     I.appendField('Home Address', '509 Shadow Oaks Dri');
-//     I.appendField('City *', 'Easley');
+//     I.appendField('Home Address', '600 Dexter Aven');
+//     I.appendField('City *', 'Montgomery');
 //     I.click('State');
-//     I.appendField('State', 'SC');
-//     I.click('SC');
-//     I.appendField('Zip Code *', '29642');
-//     I.appendField('Last Name *', 'Mitchell');
+//     I.click('#state');
+//     I.appendField('Zip Code *', '36130');
+//     I.click('Work Location');
+//     I.click('#city');
 //     I.see('That address does not exist!');
 //     I.clearField('Home Address');
 //     I.clearField('City *');
 //     I.clearField('Zip Code *');
-//     I.appendField('Home Address', '509 Shadow Oaks Dr');
-//     I.appendField('City *', 'Easle');
+//     I.appendField('Home Address', '600 Dexter Ave');
+//     I.appendField('City *', 'Montgom');
 //     I.click('State');
-//     I.appendField('State', 'SC');
-//     I.click('SC');
-//     I.appendField('Zip Code *', '29642');
-//     I.appendField('Last Name *', 'Mitchell');
+//     I.click('#state');
+//     I.appendField('Zip Code *', '36130');
+//     I.click('Work Location');
+//     I.click('#city');
 //     I.see('That address does not exist!');
 //     I.clearField('Home Address');
 //     I.clearField('City *');
 //     I.clearField('Zip Code *');
-//     I.appendField('Home Address', '509 Shadow Oaks Dr');
-//     I.appendField('City *', 'Easley');
+//     I.appendField('Home Address', '600 Dexter Ave');
+//     I.appendField('City *', 'Montgomery');
 //     I.click('State');
-//     I.appendField('State', 'SC');
-//     I.click('SC');
-//     I.appendField('Zip Code *', 'fsdf');
-//     I.appendField('Last Name *', 'Mitchell');
-//     I.see('*Required. Zip Code Not Valid.');
+//     I.click('#state');
+//     I.appendField('Zip Code *', '3613');
+//     I.click('Work Location');
+//     I.click('#city');
 //     I.see('That address does not exist!');
 //     I.clearField('Home Address');
 //     I.clearField('City *');
 //     I.clearField('Zip Code *');
-//     I.appendField('Home Address', '509 Shadow Oaks Dr');
-//     I.appendField('City *', 'Easley');
+//     I.appendField('Home Address', '600 Dexter Ave');
+//     I.appendField('City *', 'Montgomery');
 //     I.click('State');
-//     I.appendField('State', 'SC');
-//     I.click('SC');
-//     I.appendField('Zip Code *', '29642');
-//     I.appendField('Last Name *', 'Mitchell');
+//     I.click('#state');
+//     I.appendField('Zip Code *', '36130');
+//     I.click('Work Location');
+//     I.click('#city');
 //     I.dontSee('That address does not exist!');
 // });
 
 // DOESN'T WORK YET
 // should work but need username validation to work first
-// Scenario('testing valid username', (I) => {
-//     I.amOnPage('/');
-//     I.click('#signupid');
-//     I.click('Username');
-//     I.appendField('Username', 'gpichmann0');
-//     I.appendField('Last Name *', 'Mitchell');
-//     I.see('Username is unavailable!');
-//     I.clearField('Username');
-//     I.appendField('Username', 'mitchelltim29');
-//     I.clearField('Last Name *');
-//     I.appendField('Last Name *', 'Mitchell');
-//     I.dontSee('Username is unavailable!');
-// });
+Scenario('testing valid username', (I) => {
+    I.amOnPage('/');
+    I.click('#signupid');
+    I.appendField('#username', 'gpichmann0');
+    I.appendField('Last Name *', 'Mitchell');
+    I.see('Username is unavailable!');
+    I.clearField('#username');
+    I.appendField('#username', 'mitchelltim');
+    I.clearField('Last Name *');
+    I.appendField('Last Name *', 'Mitchell');
+    I.dontSee('Username is unavailable!');
+});
 
 // Scenario('testing valid and matching passwords', (I) => {
 //     I.amOnPage('/');
@@ -157,31 +140,32 @@ Feature('sign-up');
 // });
 
 // DOESN'T WORK YET
-// should work but need register form to work first
-// Scenario('testing correct sign up', (I) => {
-//     I.amOnPage('/');
-//     I.click('#signupid');
-//     I.appendField('First Name *', 'Timothy');
-//     I.appendField('Last Name *', 'Mitchell');
-//     I.appendField('Email *', 'mitchelltim29@gmail.com');
-//     I.appendField('Phone Number *', '7025952564');
-//     I.appendField('Home Address', '509 Shadow Oaks Dr');
-//     I.appendField('City *', 'Easley');
-//     // figure out how to do state
-//     I.appendField('Zip Code *', '29642');
-//     I.appendField('#username', 'mitchelltim29');
-//     I.appendField('Password *', 'password');
-//     I.appendField('Confirm Password *', 'password');
-//     I.click('Work Location');
-//     I.click('#city');
-//     I.click('Register');
-//     I.click('#signup-btn');
-//     I.click('Login');
-//     I.fillField('Username', 'mitchelltim29');
-//     I.fillField('Password', 'password');
-//     I.click('#sign-in-btn');
-//     I.dontSee('Login');
-//     I.dontSee('User not found!');
-//     I.dontSee('Username');
-//     I.dontSee('Password');
-// });
+// needs register form to work first
+Scenario('testing correct sign up', (I) => {
+    I.amOnPage('/');
+    I.click('#signupid');
+    I.appendField('First Name *', 'Timothy');
+    I.appendField('Last Name *', 'Mitchell');
+    I.appendField('Email *', 'mitchelltim29@gmail.com');
+    I.appendField('Phone Number *', '7025952564');
+    I.appendField('Home Address', '600 Dexter Ave');
+    I.appendField('City *', 'Montgomery');
+    I.click('State');
+    I.click('#state');
+    I.appendField('Zip Code *', '36130');
+    I.appendField('#username', 'mitchelltim29');
+    I.appendField('Password *', 'password');
+    I.appendField('Confirm Password *', 'password');
+    I.click('Work Location');
+    I.click('#city');
+    I.click('Register');
+    I.click('#signup-btn');
+    I.click('Login');
+    I.fillField('Username', 'mitchelltim29');
+    I.fillField('Password', 'password');
+    I.click('#sign-in-btn');
+    I.dontSee('Login');
+    I.dontSee('User not found!');
+    I.dontSee('Username');
+    I.dontSee('Password');
+});
